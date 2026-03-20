4099ebd6a262ec43bbe46b6a1d4c9f12009508a1
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-20 21:26:07 +0900

    Add analyze npm script

--- application/client/vite.config.ts
+++ application/client/vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import analyze from "vite-bundle-analyzer";
 
 export default defineConfig({
-  plugins: [nodePolyfills(), react(), tailwindcss()],
+  plugins: [
+    nodePolyfills(),
+    react(),
+    tailwindcss(),
+    ...(process.env["VITE_ANALYZE"] === "true" ? [analyze()] : []),
+  ],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
