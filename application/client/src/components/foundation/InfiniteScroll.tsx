66707b243c543fe1ca4c8a3bd38d183f73f57f3d
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-21 14:46:46 +0900

    Use IntersectionObserver

--- application/client/src/components/foundation/InfiniteScroll.tsx
+++ application/client/src/components/foundation/InfiniteScroll.tsx
@@ -8,40 +8,31 @@ interface Props {
 
 export const InfiniteScroll = ({ children, fetchMore, items }: Props) => {
   const latestItem = items[items.length - 1];
-
-  const prevReachedRef = useRef(false);
+  const bottomMarkerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const handler = () => {
-      const hasReached =
-        window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
-
-      // 画面最下部にスクロールしたタイミングで、登録したハンドラを呼び出す
-      if (hasReached && !prevReachedRef.current) {
-        // アイテムがないときは追加で読み込まない
-        if (latestItem !== undefined) {
+    if (!bottomMarkerRef.current) {
+      return;
+    }
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const isIntersecting = entries.some((entry) => entry.isIntersecting);
+        if (isIntersecting && latestItem !== undefined) {
           fetchMore();
         }
-      }
-
-      prevReachedRef.current = hasReached;
-    };
-
-    // 最初は実行されないので手動で呼び出す
-    prevReachedRef.current = false;
-    handler();
-
-    document.addEventListener("wheel", handler, { passive: false });
-    document.addEventListener("touchmove", handler, { passive: false });
-    document.addEventListener("resize", handler, { passive: false });
-    document.addEventListener("scroll", handler, { passive: false });
+      },
+      { threshold: 1.0 },
+    );
+    observer.observe(bottomMarkerRef.current);
     return () => {
-      document.removeEventListener("wheel", handler);
-      document.removeEventListener("touchmove", handler);
-      document.removeEventListener("resize", handler);
-      document.removeEventListener("scroll", handler);
+      observer.disconnect();
     };
-  }, [latestItem, fetchMore]);
+  }, [fetchMore, latestItem]);
 
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <div ref={bottomMarkerRef} aria-hidden />
+    </>
+  );
 };
