ed481065227d7c2f53f98613a12a9fe73b560faa
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-21 15:43:08 +0900

    Fix profile background color

--- application/client/src/components/user_profile/UserProfileHeader.tsx
+++ application/client/src/components/user_profile/UserProfileHeader.tsx
@@ -1,4 +1,4 @@
-import { ReactEventHandler, useCallback, useState } from "react";
+import { SyntheticEvent, useCallback, useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getProfileImagePath } from "@web-speed-hackathon-2026/client/src/utils/get_path";
@@ -13,11 +13,10 @@ export const UserProfileHeader = ({ user }: Props) => {
   const [averageColor, setAverageColor] = useState<string | null>(null);
 
   // 画像の平均色を取得します
-  /** @type {React.ReactEventHandler<HTMLImageElement>} */
-  const handleLoadImage = useCallback<ReactEventHandler<HTMLImageElement>>(async (ev) => {
+  const handleLoadImage = useCallback(async (ev: SyntheticEvent<HTMLImageElement>) => {
     const { FastAverageColor } = await import("fast-average-color");
     const fac = new FastAverageColor();
-    const { rgb } = fac.getColor(ev.currentTarget, { mode: "precision" });
+    const { rgb } = await fac.getColorAsync(ev.target as HTMLImageElement, { mode: "speed" });
     setAverageColor(rgb);
     fac.destroy();
   }, []);
@@ -25,7 +24,14 @@ export const UserProfileHeader = ({ user }: Props) => {
   return (
     <header className="relative">
       <div
-        className={`h-32 ${averageColor ? `bg-[${averageColor}]` : "bg-cax-surface-subtle"}`}
+        className={`h-32 ${averageColor ? `bg-(--calculated-color)` : "bg-cax-surface-subtle"}`}
+        style={
+          averageColor
+            ? {
+                "--calculated-color": averageColor,
+              }
+            : {}
+        }
       ></div>
       <div className="border-cax-border bg-cax-surface-subtle absolute left-2/4 m-0 h-28 w-28 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border sm:h-32 sm:w-32">
         <img
